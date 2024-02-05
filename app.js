498fc6eafeaa8f13ef614d1c0702248dbff2b48b
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2024-02-05 03:24:07 -0500

    init project

---
+++ app.js
@@ -0,0 +1,47 @@
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+let carouselDom = document.querySelector(".carousel");
+let listItemDom = document.querySelector(".carousel .list");
+let thumbnailDom = document.querySelector(".carousel .thumbnail");
+let timeRunning = 1500;
+let timeAuto = 5000;
+let runTimeOut;
+
+let runAuto = setTimeout(() => {
+  nextDom.click();
+}, timeAuto);
+
+nextDom.onclick = function () {
+  showSlider("next");
+};
+
+prevDom.onclick = function () {
+  showSlider("prev");
+};
+
+function showSlider(type) {
+  let itemSlider = document.querySelectorAll(".carousel .list .item");
+  let itemThumbnail = document.querySelectorAll(".carousel .thumbnail .item");
+
+  if (type === "next") {
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.add("next");
+  } else {
+    let positionLastItem = itemSlider.length - 1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDom.classList.add("prev");
+  }
+
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("next");
+    carouselDom.classList.remove("prev");
+  }, timeRunning);
+
+  clearTimeout(runAuto);
+  runAuto = setTimeout(() => {
+    nextDom.click();
+  }, timeAuto);
+}
